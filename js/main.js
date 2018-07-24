90ae4e4d40c046772488d69b53791b64e9697241
7Red4 <40208491+7Red4@users.noreply.github.com>
2018-07-24 22:52:17 +0800

    3

--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ $(document).ready(function(){
         }, 800);
     });
 
+    $('.skillItem1').show();
+    $('.skillItem2').hide();
+    $('.btn1').addClass('clicked');
+//手機板NAV
+    
+$('.icon').click(function(){
+    $('.mNav').slideToggle();
+});
+$(window).scroll(function(){
+    $('.mNav').slideUp('normal');
+});
+
 //照片輪播
         setInterval(carousel, 4000);
     var index = 1;
@@ -30,71 +42,81 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if ($(window).scrollTop() >= $('.profile').offset().top-100) {
          $('.nav').addClass('fixed');
+         $('.phoneBar').show();
          $('.phoneBar').addClass('fixed');
          $('.mNav').addClass('mfixed');
         } else {
          $('.nav').removeClass('fixed');
+         $('.phoneBar').hide();
          $('.phoneBar').removeClass('fixed');
          $('.mNav').removeClass('mfixed');
         }
     });
 
     $(window).scroll(function(){
-        if ($(window).scrollTop() >= $('.ability').offset().top-600) {
-            $('#codItem1').addClass('HTML');
+        if ($(window).scrollTop() >= $('#skills').offset().top-600) {
+            $('.codItem1').addClass('HTML');
         }
-        if ($(window).scrollTop() >= $('.ability').offset().top-560) {
-            $('#codItem2').addClass('CSS');
+        if ($(window).scrollTop() >= $('#skills').offset().top-560) {
+            $('.codItem2').addClass('CSS');
         }
-        if ($(window).scrollTop() >= $('.ability').offset().top-520) {
-            $('#codItem3').addClass('JavaScript');
+        if ($(window).scrollTop() >= $('#skills').offset().top-520) {
+            $('.codItem3').addClass('JavaScript');
         }
-        if ($(window).scrollTop() >= $('.ability').offset().top-480) {
-            $('#codItem4').addClass('jQuery');
+        if ($(window).scrollTop() >= $('#skills').offset().top-480) {
+            $('.codItem4').addClass('jQuery');
         }
-        if ($(window).scrollTop() >= $('.ability').offset().top-440) {
-            $('#codItem5').addClass('Vuejs');
+        if ($(window).scrollTop() >= $('#skills').offset().top-440) {
+            $('.codItem5').addClass('Vuejs');
         }
     });
     
     $(window).scroll(function(){
-        if ($(window).scrollTop() >= $('.ability').offset().top-600) {
-            $('#exItem1').addClass('Ae');
+        if ($(window).scrollTop() >= $('#skills').offset().top-600) {
+            $('.exItem1').addClass('Ae');
         }
-        if ($(window).scrollTop() >= $('.ability').offset().top-560) {
-            $('#exItem2').addClass('Pr');
+        if ($(window).scrollTop() >= $('#skills').offset().top-560) {
+            $('.exItem2').addClass('Pr');
         }
-        if ($(window).scrollTop() >= $('.ability').offset().top-520) {
-            $('#exItem3').addClass('Ps');
+        if ($(window).scrollTop() >= $('#skills').offset().top-520) {
+            $('.exItem3').addClass('Ps');
         }
-        if ($(window).scrollTop() >= $('.ability').offset().top-480) {
-            $('#exItem4').addClass('Ai');
+        if ($(window).scrollTop() >= $('#skills').offset().top-480) {
+            $('.exItem4').addClass('Ai');
         }
     });
 
-//手機板NAV
-    
-    $('.icon').click(function(){
-        $('.mNav').slideToggle();
-    })
+//技能表分頁
+    $('.btn1').click(function(){
+        $('.skillItem2').hide();
+        $('.skillItem1').show();
+        $('.btn1').addClass('clicked');
+        $('.btn2').removeClass('clicked');
+});
+    $('.btn2').click(function(){
+        $('.skillItem1').hide();
+        $('.skillItem2').show();
+        $('.btn2').addClass('clicked');
+        $('.btn1').removeClass('clicked');
+    });
 
 //作品集上字
     $('.ookami').hover(function(){
-        $(this).append($('<span>狼少年的獨白</span>'));
+        $(this).append($('<a>狼少年的獨白</a>'));
     }
     , function() {
-        $( this ).find( "span:last" ).remove();
+        $( this ).find( "a:last" ).remove();
     })
     $('.drumkit').hover(function(){
-        $(this).append($('<span>網頁簡易鼓組</span>'));
+        $(this).append($('<a>網頁簡易鼓組</a>'));
     }
     , function() {
-        $( this ).find( "span:last" ).remove();
+        $( this ).find( "a:last" ).remove();
     })
     $('.vueCal').hover(function(){
-        $(this).append($('<span>Vue計算機</span>'));
+        $(this).append($('<a>Vue計算機</a>'));
     }
     , function() {
-        $( this ).find( "span:last" ).remove();
+        $( this ).find( "a:last" ).remove();
     })
 });
