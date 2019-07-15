c91870cb322b436c347ea379a6bcdb818950f651
Red <abc401344@gmail.com>
2019-07-15 11:18:59 +0800

    w1/some details

--- f2echallange2019/w1/main.js
+++ f2echallange2019/w1/main.js
@@ -6,6 +6,27 @@ $(() => {
         newMission: "",
         missionlists: ["SECOND", "THIRD", "FORTH"]
       };
+    },
+    mounted() {
+      window.addEventListener("resize", this.handleResize);
+      this.handleResize();
+    },
+    beforeDestroy() {
+      window.removeEventListener("resize", this.handleResize);
+    },
+    methods: {
+      handleResize() {
+        let svg = $(".v-progress-circular svg");
+        let w = svg.width();
+        let h = svg.height();
+        $(".v-progress-circular__info").css({
+          width: `${w + 2}px`,
+          height: `${w + 2}px`
+        });
+        $(".v-progress-circular").css({
+          height: `${w}px`
+        });
+      }
     }
   });
 });
