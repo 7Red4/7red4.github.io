2c5372ad122e89b744c5a8b1e147958486bc2492
red <abc401344@gmail.com>
2020-08-05 00:13:59 +0800

    init vue 3

---
+++ src/components/myComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>{{ msg }}</h2>
+  <p>prop: {{ message }}</p>
+  <h2>watch me reverse!! :</h2>
+  <h2>&nbsp;{{ reverseMsg }}</h2>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  props: {
+    message: String
+  },
+
+  setup(props) {
+    const msg = "Hello it,s me Vue 3!!";
+
+    const reverseMsg = computed(() => [...props.message].reverse().join(""));
+
+    return {
+      msg,
+      reverseMsg
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <my-component :message="msg" />
+    <input v-model="msg" />
+    <button v-if="!!msg" @click="msg = ''">X</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+import myComponent from "../components/myComponent.vue";
+
+export default {
+  name: "Home",
+  components: { myComponent },
+
+  setup() {
+    const msg = ref("");
+
+    return { msg };
+  }
+};
+</script>
